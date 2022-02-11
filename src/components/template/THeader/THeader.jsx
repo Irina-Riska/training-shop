85846c3be2c25537e2756aa066c4462ab6df1f72
Irina <nuorri@gmail.com>
2022-02-11 11:17:09 +0300

    create Header

---
+++ src/components/template/THeader/THeader.jsx
@@ -0,0 +1,17 @@
+import react from "react";
+import ONavigationBar from "../../organism/ONavigationBar/ONavigationBar";
+import OTopBar from "../../organism/OTopBar/OTopBar";
+
+
+const THeader =() =>{
+
+
+    return (
+        <div>
+<ONavigationBar />
+<OTopBar />
+        </div>
+    )
+}
+
+export default THeader;
